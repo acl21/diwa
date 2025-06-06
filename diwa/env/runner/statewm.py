50e3c3c552303a1a737821dfd568a76681b8fa71
acl21 <14245491+acl21@users.noreply.github.com>
2025-06-06 19:29:48 +0200

    even better env runners

--- diwa/env/runner/statewm.py
+++ diwa/env/runner/statewm.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+from env.runner.base_runner import BaseEnvRunner
 import numpy as np
 import torch
 
@@ -9,27 +10,9 @@ import wandb
 log = logging.getLogger(__name__)
 
 
-class EnvRunner(object):
+class EnvRunner(BaseEnvRunner):
     def __init__(self):
-        self.n_envs = None
-        self.n_render = None
-        self.n_steps = None
-        self.use_wandb = None
-        self.render_dir = None
-        self.reset_at_iteration = None
-        self.act_steps = None
-        self.render_video = None
-        self.best_reward_threshold_for_success = None
-
-    def init_values(self, cfg):
-        self.n_envs = cfg.env.n_envs
-        self.n_render = cfg.env.n_render
-        self.n_steps = cfg.env.max_episode_steps
-        self.use_wandb = cfg.wandb is not None
-        self.act_steps = cfg.act_steps
-        self.render_video = cfg.env.save_video
-        self.best_reward_threshold_for_success = cfg.env.best_reward_threshold_for_success
-        self.env_type = cfg.env_type
+        super().__init__()
 
     @torch.no_grad()
     def run(self, epoch, model, venv, wme):
