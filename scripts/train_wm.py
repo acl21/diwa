b45e0893b26c89761b23619120aa98f5e4814e86
acl21 <14245491+acl21@users.noreply.github.com>
2026-02-25 17:51:05 +0100

    224 training ddp action chunk

--- scripts/train_wm.py
+++ scripts/train_wm.py
@@ -19,6 +19,8 @@ from lumos.utils.info_utils import (
     setup_logger,
     setup_tensor_cores,
 )
+# To use the mul resolver in the config file
+OmegaConf.register_new_resolver("mul", lambda x, y: x * y)
 
 # This is for using the locally installed repo clone when using slurm
 sys.path.insert(0, Path(__file__).absolute().parents[1].as_posix())
